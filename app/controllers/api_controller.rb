e4b959a897624d268da65bd59e4960114190f3f9
Robert Hutchinson <robert.hutchinson@gmail.com>
2015-03-26 17:21:12 -0400

    Starting work on games controller

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,7 @@
+class ApiController < ActionController::Base
+  before_action :hardcode_json_format
+
+  def hardcode_json_format
+    request.format = :json
+  end
+end
---
+++ app/controllers/v1/games_controller.rb
@@ -0,0 +1,15 @@
+module V1
+
+  class GamesController < ApiController
+
+    def create
+      binding.pry
+      head :ok
+    end
+
+    def list
+    end
+
+  end
+
+end
