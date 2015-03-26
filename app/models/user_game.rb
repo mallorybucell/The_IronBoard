3a20d759110914fee0c1021c70ca50ec3b8043b0
Mallory Bucell <m.f.bucell@gmail.com>
2015-03-26 18:19:25 -0400

    added validation and sketch out factories and tests

--- app/models/user_game.rb
+++ app/models/user_game.rb
@@ -1,4 +1,6 @@
 class UserGame < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
+
+  validates_uniqueness_of :user, scope: :game
 end
--- spec/models/user_game_spec.rb
+++ spec/models/user_game_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe UserGame, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can not have the same user more than once per game' do
+    u1 = FactoryGirl.create :user
+    u2 = FactoryGirl.create :user
+    u3 = FactoryGirl.create :user
+    g1 = FactoryGirl.create :game
+  end
 end
