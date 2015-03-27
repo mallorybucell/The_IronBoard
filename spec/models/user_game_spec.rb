55a95d830ad9b56ef30b5f9c064d234c329830c1
Mallory Bucell <m.f.bucell@gmail.com>
2015-03-26 21:43:21 -0400

    user_game uniquness validation test pass

--- spec/models/user_game_spec.rb
+++ spec/models/user_game_spec.rb
@@ -4,7 +4,15 @@ RSpec.describe UserGame, type: :model do
   it 'can not have the same user more than once per game' do
     u1 = FactoryGirl.create :user
     u2 = FactoryGirl.create :user
-    u3 = FactoryGirl.create :user
     g1 = FactoryGirl.create :game
+
+    UserGame.create!(user_id: u1.id, game_id: g1.id)
+    UserGame.create!(user_id: u2.id, game_id: g1.id)
+
+    expect(UserGame.count).to eq 2
+
+    expect do
+      UserGame.create!(user_id: u1.id, game_id: g1.id)
+    end.to raise_error 
   end
 end
