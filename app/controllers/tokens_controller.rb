c305cd4f98009ae2bf011c12b7d02c31268f0ed7
Mallory Bucell <m.f.bucell@gmail.com>
2015-03-26 20:01:59 -0400

    tokens controller and model methods

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,3 +1,15 @@
 class TokensController < ApplicationController
   before_filter :authenticate_admin!
+
+  def create
+    current_admin.generate_api_token!
+    redirect_to :back, notice: "Token generated"
+  end
+
+  def destroy
+    token = current_admin.tokens.find params[:id]
+    token.disable!
+    redirect_to :back, notice: "Token disabled"
+  end
+
 end
--- app/models/token.rb
+++ app/models/token.rb
@@ -1,2 +1,17 @@
 class Token < ActiveRecord::Base
+  class Invalid < StandardError; end
+  belongs_to: :admin
+
+  validates :key, presence: true, uniqueness: true
+
+  scope :valid, -> { where(active: true).where("expires_at > ?", Time.now) }
+
+  def disable!
+    update active: false
+  end
+
+  def disabled?
+    !active?
+  end
 end
+
