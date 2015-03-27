29ad4d9c12b79f37ada62404dd347d0659108d3a
Mallory Bucell <m.f.bucell@gmail.com>
2015-03-27 10:33:55 -0400

    test to make sure user can be in 2 different games

--- spec/models/user_game_spec.rb
+++ spec/models/user_game_spec.rb
@@ -5,11 +5,16 @@ RSpec.describe UserGame, type: :model do
     u1 = FactoryGirl.create :user
     u2 = FactoryGirl.create :user
     g1 = FactoryGirl.create :game
+    g2 = FactoryGirl.create :game
 
     UserGame.create!(user_id: u1.id, game_id: g1.id)
     UserGame.create!(user_id: u2.id, game_id: g1.id)
 
     expect(UserGame.count).to eq 2
+
+    UserGame.create!(user_id: u1.id, game_id: g2.id)
+    expect(UserGame.count).to eq 3
+    expect(u1.games.count).to eq 2
 
     expect do
       UserGame.create!(user_id: u1.id, game_id: g1.id)
